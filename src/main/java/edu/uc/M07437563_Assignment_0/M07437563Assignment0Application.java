c45b8a9d2815110335841f0ce1aa7dace0af6b76
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-26 22:55:31 -0400

    initialize

---
+++ src/main/java/edu/uc/M07437563_Assignment_0/M07437563Assignment0Application.java
@@ -0,0 +1,13 @@
+package edu.uc.M07437563_Assignment_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M07437563Assignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M07437563Assignment0Application.class, args);
+	}
+
+}
